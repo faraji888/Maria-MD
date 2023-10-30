129ce49c2c21964ee0d67c17177c83b66aebf281
faraji888 <148947189+faraji888@users.noreply.github.com>
2023-10-30 10:46:32 +0100

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,37 +2,39 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "919931122319"
-global.ownername = "Ayush pandey"
-global.ytname = "YT: AyushBotz.inc"
-global.socialm = "GitHub: Ayushpandey023"
-global.location = "Indian"
+global.ownernomer = "4593707507"
+global.ownername = "monster xxx"
+global.ytname = "YT: monster.inc"
+global.socialm = "GitHub: monster"
+global.location = "marocco"
 
-global.ownernumber = '919931122319'  //creator number
-global.ownername = 'Ayush' //owner name
-global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
+global.ownernumber = '4593707507'  //creator number
+global.ownername = '𝑵𝑨𝑹𝑪𝑰𝑺𝑺𝑰𝑺𝑻𝑰𝑪' //owner name
+global.botname = '
+#𝐆𝐁𝐗 /#/ 𝑵𝑨𝑹𝑪𝑰𝑺𝑺𝑰𝑺𝑻𝑰𝑪 !-𝐌𝐃' //name of the bot
 
 //sticker details
-global.stickername = '💙𓆩‎✘AʏᴜSʜ✘𓆪_✰'
+global.stickername = '
+#𝐆𝐁𝐗 /#/ 𝑵𝑨𝑹𝑪𝑰𝑺𝑺𝑰𝑺𝑻𝑰𝑪 !⚔️'
 global.packname = 'Sticker By'
-global.author = 'Maria Bot'
+global.author = 'NARCISSIST BOT'
 //console view/theme
-global.themeemoji = '🧩'
-global.wm = "Ayush botz inc."
+global.themeemoji = '👻'
+global.wm = "NARCISSIST BOT."
 
 //theme link
 global.link = '' // if you want to send bot reply with group link then put your link here 🌿 
-global.support = 'https://chat.whatsapp.com/DYp7FwfL9xsKURihcdT0l8'
+global.support = 'wa.me//4593707507'
 //custom prefix
 global.prefa = ['.'] //put here your fav prefix like +×÷= etc.
 
 //false=disable and true=enable
-global.autoRecording = false //auto recording
-global.autoTyping = false //auto typing
-global.autorecordtype = false //auto typing + recording
+global.autoRecording = true //auto recording
+global.autoTyping = true //auto typing
+global.autorecordtype = true //auto typing + recording
 global.autoread = true //auto read messages
 global.autobio = false //auto update bio
-global.anti92 = true //auto block +92 
+global.anti92 = true //auto block +91
 global.autoread_status = true //auto view status/story
 
 
@@ -40,7 +42,7 @@ global.autoread_status = true //auto view status/story
 //reply messages
 global.mess = {
     done: 'Done !',
-    pm: ' 📍*sorry darling* Bot wont work in private darling📍 \n\n https://chat.whatsapp.com/FoS7pSPtfMqBuoireK4aAJ',
+    pm: ' 📍*sorry darling* Bot wont work in private darling📍 \n\n wa.me//4593707507',
     prem: 'This feature can be used by premium user only',
     admin: 'This feature can be used by admin only',
     botAdmin: 'This feature can only be used when the bot is a group admin ',
